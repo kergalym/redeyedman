070d6a7b8c7420c438f7872caa557ad0f901ca4f
Kerimbekov Galym <kegalym2@mail.ru>
2019-12-14 23:30:01 +0600

    Some fixes.

--- application/core/servicegate.py
+++ application/core/servicegate.py
@@ -52,5 +52,7 @@ class Backup:
                 tar.add(self.site_parent_path)
             return "Backup is created: {}".format(self.file_output)
         else:
-            return "No correct data for export the database or destination/site parent path isn't exist. " \
-                   "Please, contact to administrator"
+            return ("No correct data for export the database or destination/site parent path isn't exist.\n"
+                    "Backup Path: {}\n".format(self.backup_path),
+                    "Backup Path Access: {}\n".format(os.access(self.backup_path, os.W_OK)),
+                    "Please, contact to administrator \n")
